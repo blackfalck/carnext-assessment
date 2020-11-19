5a4ad05b8a37f0a2a5b5397e67857dd627027276
Mitchell Izelaar <>
2020-11-19 14:56:02 +0100

    Create database by default

--- docker/install.sh
+++ docker/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 docker-compose run --rm php composer install
-docker-compose run --rm php carnext/bin/console doctrine:migrations:migrate
+docker-compose run --rm php bin/console doctrine:migrations:migrate
 
 docker-compose run --rm angular npm install
